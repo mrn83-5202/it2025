e4ee6621adc7b464800b15f88a514e95a58615ad
= <=>
2025-01-20 15:32:13 +0200

    ++

---
+++ DB/query1.sql
@@ -0,0 +1,20 @@
+-- Створення таблиці
+CREATE TABLE personal (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    surname VARCHAR(50) NOT NULL,
+    rank VARCHAR(50) NOT NULL,
+    position VARCHAR(100) NOT NULL
+);
+
+-- Вставка даних
+INSERT INTO personal (name, surname, rank, position)
+VALUES
+('Ivan', 'Petrenko', 'Captain', 'Platoon Commander'),
+('Olena', 'Kovalenko', 'Lieutenant', 'Communications Officer'),
+('Andrii', 'Shevchenko', 'Major', 'Battalion Commander'),
+('Oksana', 'Ivchenko', 'Sergeant', 'Logistics Specialist'),
+('Mykola', 'Bondarenko', 'Colonel', 'Regimental Commander');
+
+-- Перевірка даних
+SELECT * FROM personal;
